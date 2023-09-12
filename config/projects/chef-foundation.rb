92f55c9b70edddfa0d44b639e6b9f161e501ff38
Sean Simmons <ssimmons@progress.com>
2023-09-12 10:56:37 -0400

    this cert is not in the machien store its in user
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- config/projects/chef-foundation.rb
+++ config/projects/chef-foundation.rb
@@ -91,7 +91,7 @@ package :msi do
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
   keypair-alias "key_495941360"
-  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: true
+  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: false
   parameters ProjectLocationDir: project_location_dir
 end
 
