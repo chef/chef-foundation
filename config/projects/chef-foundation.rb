0e09de7f212311e89b767b23bf28a8296f9c8c0b
Sean Simmons <ssimmons@progress.com>
2023-09-12 10:57:07 -0400

    using underscore
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- config/projects/chef-foundation.rb
+++ config/projects/chef-foundation.rb
@@ -90,7 +90,7 @@ package :msi do
   upgrade_code msi_upgrade_code
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
-  keypair-alias "key_495941360"
+  keypair_alias "key_495941360"
   signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: false
   parameters ProjectLocationDir: project_location_dir
 end
