f6ecc1e8ebb508af493139cd805b71d2479abb93
Sean Simmons <ssimmons@progress.com>
2023-11-22 14:48:57 -0500

    what the hell is cache doing
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- omnibus.rb
+++ omnibus.rb
@@ -31,8 +31,8 @@ env_omnibus_windows_arch = :x86 unless %w{x86 x64}.include?(env_omnibus_windows_
 
 windows_arch env_omnibus_windows_arch
 
-use_git_caching true
-use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
+use_git_caching false
+use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", false)
 
 # Enable S3 asset caching
 # ------------------------------
