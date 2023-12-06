9fb522baf4aabb45eb04df1ccf76e81d4755472e
muthuja <127753948+muthuja@users.noreply.github.com>
2023-12-06 20:25:07 +0530

    updated the gemfile lock (#77)
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- config/projects/chef-foundation.rb
+++ config/projects/chef-foundation.rb
@@ -93,7 +93,7 @@ package :msi do
   wix_candle_extension "WixUtilExtension"
   wix_light_extension "WixUtilExtension"
   # Update this if you start getting a "SignTool Error: No certificates were found that met all the given criteria." error in adhoc builds
-  signing_identity "13B510D1CF1B3467856A064F1BEA12D0884D2528", machine_store: true
+  signing_identity "769E6AF679126F184850AAC7C5C823A80DB3ADAA", machine_store: false, keypair_alias: "key_495941360"
   parameters ProjectLocationDir: project_location_dir
 end
 
