b87564857aaa7e4e3c1ea87fd0604434c96c11fa
Poornima <94897054+poorndm@users.noreply.github.com>
2024-01-18 16:52:47 +0530

    To fix signature issue on Notarization (#106)
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -1,6 +1,7 @@
 # THIS IS NOW HAND MANAGED, JUST EDIT THE THING
 # keep it machine-parsable since CI uses it
 #
+#
 # NOTE: You MUST update omnibus-software when adding new versions of
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
 override "libffi", version: "3.4.2"
