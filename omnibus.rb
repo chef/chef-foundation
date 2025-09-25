62e1fff9fde4189df790cf982caa4632a1d8abb1
Poornima <94897054+poorndm@users.noreply.github.com>
2025-09-25 16:22:59 +0530

    Update for s3-private-access and windows cert update (#188)
    * Modified to test s3-private access
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    * Update windows-cert
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    * Exclude AIX due to issue on s3-private-bucket access
    
    Signed-off-by: poorndm <poorndm@progress.com>
    
    ---------
    
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus.rb
+++ omnibus.rb
@@ -32,6 +32,7 @@ env_omnibus_windows_arch = :x86 unless %w{x86 x64}.include?(env_omnibus_windows_
 windows_arch env_omnibus_windows_arch
 
 use_git_caching true
+# note, this is statically set in the omnibus-buildkite-plugin, you are always going to be forced to use internal sources. If you dont want internal sources, you must enable this to false.
 use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
 
 # Enable S3 asset caching
@@ -39,10 +40,11 @@ use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
 use_s3_caching true
 s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
 s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
-s3_bucket "opscode-omnibus-cache"
-# s3_bucket      'opscode-omnibus-cache-private'
-# s3_acl         'private'
-# s3_region      'us-west-2'
+
+s3_bucket      'opscode-omnibus-cache-private'
+s3_acl         'private'
+s3_region      'us-west-2'
+
 
 build_retries 3
 fetcher_retries 3
